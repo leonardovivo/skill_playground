b0fe0a2e25dde04535ba942f480a9be7091ef8fe
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-08-01 23:54:50 -0300

    implementa logica do estado

---
+++ lib/blocs/counter_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class CounterEvent {}
+
+class IncrementEvent extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<IncrementEvent>((event, emit) {
+      emit(state + 1);
+    });
+  }
+}
