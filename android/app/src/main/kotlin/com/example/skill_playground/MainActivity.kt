0f876152c97ed43707610ea44a6e44cf3716c098
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-08-02 20:50:10 -0300

    implementa metodo para retornar mensagem nativa no Android

--- android/app/src/main/kotlin/com/example/skill_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/skill_playground/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.example.skill_playground
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.exemplo/native"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "getMessage") {
+                result.success("Mensagem do Android")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
