68fe44f6779d924970b76656a6787de1d0ecd40c
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-08-03 01:32:44 -0300

    adiciona tela de performance com comparação de repaintboundary

---
+++ lib/screens/performance/performance_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:skill_playground/screens/performance/widgets/grid_animated.dart';
+
+class PerformanceScreen extends StatelessWidget {
+  const PerformanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          title: const Text(
+            'RepaintBoundary',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Column(
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'Esquerda: sem RepaintBoundary\nDireita: com RepaintBoundary',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: GridAnimated(useRepaintBoundary: false)),
+                  Expanded(child: GridAnimated(useRepaintBoundary: true)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
