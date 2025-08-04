00b134b76e38df8ac26fc8c24d4205d8da662ff3
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-08-04 01:08:44 -0300

    cria tela do animation controller

---
+++ lib/screens/animation/animation_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:skill_playground/widgets/animated_ball.dart';
+
+class AnimationScreen extends StatelessWidget {
+  const AnimationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+          title: const Text(
+            'AnimationController',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: const AnimatedBall(),
+      ),
+    );
+  }
+}
